03709de808d0d70471791bcf4fa8bfa336aa7479
j-schnell <jschnell@uwaterloo.ca>
2023-08-08 19:03:26 -0400

    Improve inference

--- dataset_generation.py
+++ dataset_generation.py
@@ -1,40 +1,59 @@
+import argparse
 import sys
 import torch
-from data import PascalSegmentationDataset, ADE20KDataset
+from data import PascalSegmentationDataset, PascalScribbleDataset, ADE20KDataset
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 sys.path.append('/home/jacob/scribblegen/ControlNet')
 from ControlNet.cldm.model import create_model, load_state_dict
 
-device = torch.device('cuda:2')
-dataset = PascalSegmentationDataset(train=True)
+def main():
+    # Set these args:
+    parser = argparse.ArgumentParser(description="PyTorch DeeplabV3Plus Training")
+    parser.add_argument('--checkpoint', type=str, default=None, required=True,
+                        help='path of the model checkpoint for inference')
+    parser.add_argument('--out-dir', type=str, default=None, required=True,
+                        help='directory for output images')
+    parser.add_argument('--gpu-id', type=int, default=0,
+                        help='which gpu to use for inference')
+    parser.add_argument('--dataset', type=str, default='PascalScribble',
+                        choices=['ADE20K', 'PascalSegmentation', 'PascalScribble'],
+                        help='dataset to run inference on')
+    parser.add_argument('--add-hint', action='store_true', default=False,
+                        help='whether to add class hints to prompts')
+    args = parser.parse_args()
 
-model_config_path = './ControlNet/models/cldm_v15.yaml'
-model_checkpoint_path = 'logs/PascalSegmentation/lightning_logs/version_15/checkpoints/epoch=126-step=84073.ckpt'
+    device = torch.device(f'cuda:{args.gpu_id}')
+    dataset = PascalScribbleDataset(train=True, class_hint={args.add_hint})
+    out_dir = args.out_dir
 
-data_path = 'temp_data/'
+    model_config_path = './ControlNet/models/cldm_v15.yaml'
+    os.makedirs(out_dir, exist_ok=True)
+    assert len(os.listdir(out_dir)) == 0
 
-model = create_model(model_config_path).to(device)
-model.load_state_dict(load_state_dict(model_checkpoint_path, location=device))
-model.cond_stage_model.device = device
+    model = create_model(model_config_path).to(device)
+    model.load_state_dict(load_state_dict(args.checkpoint, location=device))
+    model.cond_stage_model.device = device
 
-for i, data in enumerate(dataset):
-    data['hint'] = torch.tensor(data['hint'], device=device).unsqueeze(0)
-    data['jpg'] = torch.tensor(data['jpg'], device=device).unsqueeze(0)
-    data['txt'] = [data['txt']]
-    x, c = model.get_input(data, k=0)
-    log = model.log_images(data)
+    for i, data in enumerate(dataset):
+        data['hint'] = torch.tensor(data['hint'], device=device).unsqueeze(0)
+        data['jpg'] = torch.tensor(data['jpg'], device=device).unsqueeze(0)
+        data['txt'] = [data['txt']]
+        log = model.log_images(data)
 
-    img = log['samples_cfg_scale_9.00']
-    img = torch.clamp(img, -1., 1.)
-    img = img.cpu().numpy()[0]
-    img = np.moveaxis(img, 0, -1)
-    img = (img + 1) / 2
-    img = (img * 255).astype(np.uint8)
-    img = img[...,::-1]
+        img = log['samples_cfg_scale_9.00']
+        img = torch.clamp(img, -1., 1.)
+        img = img.cpu().numpy()[0]
+        img = np.moveaxis(img, 0, -1)
+        img = (img + 1) / 2
+        img = (img * 255).astype(np.uint8)
+        img = img[...,::-1]
 
-    path = data_path + data['name'] + '.jpeg'
-    print(f'Saving new image to {path} {i}/{len(dataset)}')
-    plt.imsave(path, img, format='jpeg')
-    
+        path = os.path.join(out_dir, data['name'] + '.jpeg')
+        print(f'Saving new image to {path} {i}/{len(dataset)}')
+        plt.imsave(path, img, format='jpeg')
+
+if __name__ == "__main__":
+   main()
