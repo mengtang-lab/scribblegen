9e419b04873ed3af24a7c072760f0068e845ab63
Rabbittac <jwang450@ucmerced.edu>
2024-06-06 15:35:19 -0700

    Delete TreeEnergyLoss directory

--- TreeEnergyLoss/kernels/lib_tree_filter/furnace/base_model/__init__.py
+++
@@ -1,2 +0,0 @@
-from .resnet import ResNet, resnet18, resnet34, resnet50, resnet101, resnet152
-from .xception import Xception, xception39
--- TreeEnergyLoss/lib/metrics/cityscapes/make.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# check the enviroment info
-
-PYTHON="/usr/bin/python3"
-export PYTHONPATH="/data/liangzhiyuan/projects/TEL":$PYTHONPATH
-
-cd ../../../
-${PYTHON} lib/metrics/cityscapes/setup.py build_ext --inplace
